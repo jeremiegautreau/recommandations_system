3656acf7c628f58ab3aa616afaf4680ebc1ffa7c
JG <gautreaujeremie@gmail.com>
2023-03-25 10:35:28 +0100

    Change to adapt from change in other files

--- reco_system_main.py
+++ reco_system_main.py
@@ -1,20 +1,27 @@
+#! C:/Users/jerem/AppData/Local/Programs/Python/Python310/python.exe
 
 from db_request import *
 from reco_cf_item import *
 
 def main():
 
-    db, cursor = db_connect()
+    connection = db_connect()
 
-    df = data_request(db)
+    df = data_request(connection)
 
-    user = user_request(cursor)
-
-    close_connection(db, cursor)
-
-    reco = cf_item_pearson(df, user)
+    user = user_request(connection)
     
-    return reco
+    # user = 1
+
+    df_movie = data_movie(connection)
+
+    close_connection(connection)
+
+    reco = cf_item_pearson(df, user, df_movie)
+
+    print(reco)
+    
+    # return reco
 
 if __name__ == "__main__":
     main()
