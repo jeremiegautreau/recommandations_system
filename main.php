a6b6d391993c8b559fd3ad04ce4487def7d5e8a6
JG <gautreaujeremie@gmail.com>
2023-04-10 14:23:09 +0200

    new variable for personalization

--- main.php
+++ main.php
@@ -1,11 +1,14 @@
 <?php
-    include 'retrieve_data.php'; // Get function to retrieve data from forms
-    include 'db_insertion.php'; // Get function to insert data in database
+    include 'retrieve_data.php'; // get function to retrieve data from forms
+    include 'db_insertion.php'; // get function to insert data in database
 
     $data= retrieve_data(); // retrive data from html form
 
     db_insertion($data); // insert data in the database
 
-    include 'result_reco.html'; // read html with the result of the recommandation
+    $name = $_POST["name"]; // get name from form
+    $firstname = $_POST["firstname"]; // get firstname from form
+
+    include 'testSiteRestitution.html'; // read html with the result of the recommandation
 
 ?>
